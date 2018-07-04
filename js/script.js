8007c3522440d4e7efa5aa3f3539cfe6010ae72d
jeremybowden73 <jeremybowden73.gmail.com>
2018-07-04 12:03:14 +0100

    add AJAX request for a card

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
 
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function (data) {
-    console.log(data);
-  }
-});
+// $.ajax({
+//   url: 'https://randomuser.me/api/',
+//   dataType: 'json',
+//   success: function (data) {
+//     console.log(data.results[0].email);
+//   }
+// });
+
+
+
+function createCard() {
+  const newCard = document.createElement("div");
+  newCard.className = "gridCard";
+  // create divs for the card information
+  const photoDiv = document.createElement("div");
+  photoDiv.className = "photo";
+  const nameDiv = document.createElement("div");
+  nameDiv.className = "name";
+  const emailDiv = document.createElement("div");
+  emailDiv.classList.add("email");
+  const locationDiv = document.createElement("div");
+  locationDiv.classList.add("location");
+  // drop the four elements into the 'newCard' element
+  newCard.appendChild(photoDiv);
+  newCard.appendChild(nameDiv);
+  newCard.appendChild(emailDiv);
+  newCard.appendChild(locationDiv);
+  // ajax request
+  $.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function (data) {
+      //console.log(data);
+      // populate the card elements from the ajax response
+      let firstName = data.results[0].name.first;
+      nameDiv.innerHTML = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+      emailDiv.innerHTML = data.results[0].email;
+      locationDiv.innerHTML = data.results[0].location.city;
+    }
+  });
+  //
+  return newCard;
+};
+
+
+const divGridContainer = document.querySelector(".gridContainer");
+// add 12 "grid cards" to the main grid
+divGridContainer.appendChild(createCard());
+
